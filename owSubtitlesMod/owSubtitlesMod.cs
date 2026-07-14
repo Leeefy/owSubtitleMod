9bc7b7b78bf4a4ee08d10d31d9ba5d14ac3da06b
leeefy <railontheloose@proton.me>
2026-07-14 12:52:54 +0100

    initial commit with ow-mod-template

---
+++ owSubtitlesMod/owSubtitlesMod.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace owSubtitlesMod;
+
+public class owSubtitlesMod : ModBehaviour
+{
+	public static owSubtitlesMod Instance;
+
+	public void Awake()
+	{
+		Instance = this;
+		// You won't be able to access OWML's mod helper in Awake.
+		// So you probably don't want to do anything here.
+		// Use Start() instead.
+	}
+
+	public void Start()
+	{
+		// Starting here, you'll have access to OWML's mod helper.
+		ModHelper.Console.WriteLine($"My mod {nameof(owSubtitlesMod)} is loaded!", MessageType.Success);
+
+		new Harmony("Ki_ii.owSubtitlesMod").PatchAll(Assembly.GetExecutingAssembly());
+
+		// Example of accessing game code.
+		OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+		LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+	}
+
+	public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+	{
+		if (newScene != OWScene.SolarSystem) return;
+		ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+	}
+}
+
